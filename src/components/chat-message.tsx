f4c6642eb964a80cf60f8a8eef65d128b2def89f
Piotr Karwatka <piotr@catchthetornado.com>
2024-07-16 10:05:08 +0000

    fix ui tweaks

--- src/components/chat-message.tsx
+++ src/components/chat-message.tsx
@@ -10,11 +10,13 @@ interface ChatMessageProps {
 const ChatMessage: React.FC<ChatMessageProps> = ({ message }) => {
     return (
     <div className={message.role === 'user' ?  "flex items-start gap-4 justify-end" :  "flex items-start gap-4"}>
-        <Avatar className="w-8 h-8 border">
-          <AvatarImage src="/placeholder-user.jpg" />
-          <AvatarFallback>OA</AvatarFallback>
-        </Avatar>
-        <div className="grid gap-1">
+        {message.role !== 'user' ? (
+          <Avatar className="w-8 h-8 border rounded">
+            <AvatarImage src="/placeholder-user.jpg" />
+            <AvatarFallback>AI</AvatarFallback>
+          </Avatar>
+        ) : null }
+        <div className={message.role === 'user' ?  "grid gap-1 text-right bg-gray dark:bg-zinc-200 rounded" :  "grid gap-1 rounded bg-white dark:bg-zinc-950"}>
           <div className="font-bold">{message.name}</div>
           <div className="prose text-muted-foreground">
             {message.content}
