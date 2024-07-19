1b083336ae60a353edc02c551fdc535406b3a825
Piotr Karwatka <pkarwatka@divante.pl>
2024-07-19 11:26:34 +0200

    [fix] css works for message responses

--- src/components/chat-message.tsx
+++ src/components/chat-message.tsx
@@ -12,7 +12,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ message, ref }) => {
     <div id={'msg-' + message.id} ref={ref} className={message.role === 'user' ?  "flex items-start gap-4 justify-end" :  "flex items-start gap-4"}>
         <div className={message.role === 'user' ?  "p-4 grid gap-4 text-right rounded-lg max-w-[70%] bg-gray dark:bg-zinc-500" :  "p-4 grid gap-1 rounded-lg max-w-[70%] bg-white dark:bg-zinc-950"}>
           <div className="font-bold">{message.name}</div>
-          <div className="prose text-sm text-muted-foreground">
+          <div className="prose text-sm text-muted-foreground [&>*]:p-2 [&_li]:list-disc [&_li]:ml-4">
             <Markdown>
               {message.content}
             </Markdown>
