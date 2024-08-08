d81d12c90734a6cbeb063e078d025918f61dbe65
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-08 18:26:23 +0200

    [feat] Add auto login functionality in AuthorizationGuard

--- src/components/authorization-guard.tsx
+++ src/components/authorization-guard.tsx
@@ -8,11 +8,13 @@ import { KeepLoggedinLoader } from './keep-loggedin-loader';
 const AuthorizationGuard: React.FC<PropsWithChildren> = ({ children }) => {
     const dbContext = useContext(DatabaseContext);
     const [keepLoggedIn, setKeepLoggedIn] = useState(typeof localStorage !== 'undefined' ? localStorage.getItem("keepLoggedIn") === "true" : false)
+    const [autoLoginInProgress, setAutoLoginInProgress] = useState(false);
 
     useEffectOnce(() => {
         if(keepLoggedIn) {
             const databaseId = localStorage.getItem("databaseId") as string;
             const key = localStorage.getItem("key") as string;
+            setAutoLoginInProgress(true);
             const result = dbContext?.keepLoggedIn({
                 encryptedDatabaseId: databaseId,
                 encryptedKey: key,
@@ -21,12 +23,13 @@ const AuthorizationGuard: React.FC<PropsWithChildren> = ({ children }) => {
                     if(!result?.success) {
                         setKeepLoggedIn(false);
                     }
+                    setAutoLoginInProgress(false);
                 });
             }
         });
 
     return (dbContext?.authStatus === DatabaseAuthStatus.Authorized) ? (
-        <>{children}</>) : (<AuthorizePopup autoLoginInProgress={keepLoggedIn} />);
+        <>{children}</>) : (<AuthorizePopup autoLoginInProgress={autoLoginInProgress} />);
 };
 
 export default AuthorizationGuard;
