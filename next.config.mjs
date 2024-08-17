1f49a91242c34a5e93c0efe8942bb4eed7490031
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-17 09:35:35 +0200

    [fix] React strict mode disabled - caused twice rendering all the components

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       config.module.rules.push({
         test: /\.wasm$/,
