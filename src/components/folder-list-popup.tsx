6d5404c55fb8d2bdec5502f75cf7cf9c3823b386
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-27 11:15:59 +0200

    [fix] double import removed

--- src/components/folder-list-popup.tsx
+++ src/components/folder-list-popup.tsx
@@ -5,7 +5,7 @@ import { DatabaseAuthStatus, DataLoadingStatus } from "@/data/client/models";
 import DataLoader from "./data-loader";
 import { ConfigContext } from "@/contexts/config-context";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
-import { FoldersIcon, FoldersIcon, ListIcon, PlusIcon, Terminal, User2Icon, Users, UserX2Icon } from "lucide-react";
+import { FoldersIcon, ListIcon, PlusIcon, Terminal, User2Icon, Users, UserX2Icon } from "lucide-react";
 import { Credenza, CredenzaContent, CredenzaDescription, CredenzaHeader, CredenzaTitle, CredenzaTrigger } from "./credenza";
 import { Button } from "./ui/button";
 import DatabaseLinkAlert from "./shared/database-link-alert";
