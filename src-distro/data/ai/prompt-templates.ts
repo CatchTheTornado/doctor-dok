8ec3ca4d535696da09b1b6a68878e463fdc00b68
Piotr Karwatka <pkarwatka@divante.pl>
2024-08-29 12:13:03 +0200

    [feat] prompt templates - closes #120

--- src-distro/data/ai/prompt-templates.ts
+++ src-distro/data/ai/prompt-templates.ts
@@ -3,11 +3,45 @@ import { ConfigContextType } from "@/contexts/config-context";
 type PromptTemplateContext = {
     config?: ConfigContextType | null;
 }
+const promptOptions = [
+    'healthy lifestyle (longevity)',
+    'sports performance',
+    'diabetes risk',
+    'the need for supplements',
+    'cardiovascular disease risk',
+    'cancer risk',
+    'hydration levels',
+    'hypertension (high blood pressure)',
+    'kidney function',
+    'liver function',
+    'thyroid function',
+    'hormone levels',
+    'vitamin and mineral levels',
+    'inflammation',
+    'pregnancy planning',
+    'blood system diseases',
+    'metabolic and hormonal disorders',
+    'infections and infectious diseases'
+];
+
 export const promptTemplates = {
-    recordParseMultimodal: {
-        label: "Check my latest LDL or MCV",
+    analyzeMyLatestResults: {
+        label: "Analyze my latest test results for ...",
         template: (context: PromptTemplateContext) => {
-            return "Check my latest {select:LDL|MCV} blood results and show the trends from previous ones"
+            return "Analyze my latest test results for {select:" + promptOptions.join('|') + "}"
         },
-    }
+    },
+    checkTrends: {
+        label: "Check my test results for trends",
+        template: (context: PromptTemplateContext) => {
+            return "Check my latest test results for {select:" + promptOptions.join('|') + "} and show the trends from previous ones"
+        },
+    },    
+    howCanIImprove: {
+        label: "How can I improve based on my test results?",
+        template: (context: PromptTemplateContext) => {
+            return "How can I improve my latest test results in the area of {select:" + promptOptions.join('|') + "}?"
+        },
+    },    
+
 }
