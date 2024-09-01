f2fc63093e5c8c00887e1464c1e1f79491d80646
Piotr Karwatka <pkarwatka@divante.pl>
2024-09-01 19:42:48 +0200

    [fix] format detection removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="format-detection" content="telephone=no" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no"></meta>
       </head>
       <body className={inter.className}>
