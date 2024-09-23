310316a99d7f973dc8cf6e7c9694d745497298b9
Piotr Karwatka <pkarwatka@divante.pl>
2024-09-23 19:43:52 +0200

    [fix] dependency

--- src/components/saas-context-loader.tsx
+++ src/components/saas-context-loader.tsx
@@ -12,7 +12,7 @@ export function SaaSContextLoader() {
             saasContext.setSaasToken(saasToken);
             saasContext.loadSaaSContext(saasToken);
         }
-    }, [searchParams, saasContext]);
+    }, [searchParams]);
 
     return (
         <>
