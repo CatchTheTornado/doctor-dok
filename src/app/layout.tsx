23fca542285abe344bd5d64618a0218decff9df3
Piotr Karwatka <pkarwatka@divante.pl>
2024-09-30 12:21:36 +0200

    [feat] fixed PWA icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,15 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="format-detection" content="telephone=no" />
+        <meta name="theme-color" content="#000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/180.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/img/512.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/img/144.png" />
+        <link rel="apple-touch-icon" sizes="128x128" href="/img/128.png" />
+        <link rel="apple-touch-icon" sizes="1024x1024" href="/img/1024.png" />
+        <link rel="apple-touch-icon" href="/img/1024.png" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no"></meta>
       </head>
       <body className={inter.className}>
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,14 +7,44 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Secure Data Folder folder with AI assistant',
     start_url: '/',
     display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    background_color: '#000',
+    theme_color: '#000',
     icons: [
       {
         src: '/img/doctor-dok-logo.svg',
         sizes: 'any',
         type: 'image/svg+xml',
       },
+      {
+        src: '/img/72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: '/img/128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/img/144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '/img/180.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/img/512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/img/1024.png',
+        sizes: '1024x1024',
+        type: 'image/png',
+      }
     ],
   }
 }
